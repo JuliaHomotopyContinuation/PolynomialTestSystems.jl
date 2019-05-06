5f8a2e920be211376ccb5cdfbaf65ad5adcb8017
Sascha Timme <sascha.timme@gmail.com>
2019-05-06 09:05:58 +0200

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,12 @@
 using Documenter, PolynomialTestSystems
 
 makedocs(
-    format = :html,
     sitename = "PolynomialTestSystems.jl",
     pages = [
-        "PolynomialTestSystems" => "index.md"
-        ]
+        "PolynomialTestSystems" => "index.md",
+    ]
 )
 
 deploydocs(
     repo   = "github.com/JuliaHomotopyContinuation/PolynomialTestSystems.jl.git",
-    target = "build",
-    julia = "0.6",
-    osname = "linux",
-    deps   = nothing,
-    make   = nothing
 )
